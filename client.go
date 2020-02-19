7c410ea453e9835d4d815b23a1aeb86b8d35c981
emc2314 <emc2314@mail.ustc.edu.cn>
2020-02-19 18:09:25 +0800

    Seperate http2

--- client.go
+++ client.go
@@ -157,6 +157,12 @@ func (d *Dialer) DialContext(ctx context.Context, urlStr string, requestHeader h
 		return nil, nil, err
 	}
 
+	h2 := false
+	if strings.HasSuffix(urlStr, "/h2") {
+		h2 = true
+		urlStr = strings.TrimSuffix(urlStr, "/h2")
+	}
+
 	u, err := url.Parse(urlStr)
 	if err != nil {
 		return nil, nil, err
@@ -310,7 +316,89 @@ func (d *Dialer) DialContext(ctx context.Context, urlStr string, requestHeader h
 		if cfg.ServerName == "" {
 			cfg.ServerName = hostNoPort
 		}
-		tlsConn := tls.UClient(netConn, cfg, tls.HelloChrome_Auto)
+		var tlsConn *tls.UConn
+		if h2 {
+			tlsConn = tls.UClient(netConn, cfg, tls.HelloCustom)
+			clientHelloSpec := tls.ClientHelloSpec{
+				CipherSuites: []uint16{
+					tls.TLS_AES_128_GCM_SHA256,
+					tls.TLS_AES_256_GCM_SHA384,
+					tls.TLS_CHACHA20_POLY1305_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.DISABLED_TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.DISABLED_TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+					tls.DISABLED_TLS_RSA_WITH_AES_256_CBC_SHA256,
+					tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
+					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+					0xc008,
+					tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+					tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+				},
+				CompressionMethods: []byte{
+					0x00, // compressionNone
+				},
+				Extensions: []tls.TLSExtension{
+					&tls.RenegotiationInfoExtension{Renegotiation: tls.RenegotiateOnceAsClient},
+					&tls.SNIExtension{},
+					&tls.UtlsExtendedMasterSecretExtension{},
+					&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
+						tls.ECDSAWithP256AndSHA256,
+						tls.PSSWithSHA256,
+						tls.PKCS1WithSHA256,
+						tls.ECDSAWithP384AndSHA384,
+						tls.ECDSAWithSHA1,
+						tls.PSSWithSHA384,
+						tls.PSSWithSHA384,
+						tls.PKCS1WithSHA384,
+						tls.PSSWithSHA512,
+						tls.PKCS1WithSHA512,
+						tls.PKCS1WithSHA1,
+					}},
+					&tls.StatusRequestExtension{},
+					&tls.SCTExtension{},
+					&tls.ALPNExtension{AlpnProtocols: []string{"http/1.1"}},
+					&tls.SupportedPointsExtension{SupportedPoints: []byte{
+						0x00, // pointFormatUncompressed
+					}},
+					&tls.KeyShareExtension{[]tls.KeyShare{
+						{Group: tls.X25519},
+					}},
+					&tls.PSKKeyExchangeModesExtension{[]uint8{
+						tls.PskModeDHE,
+					}},
+					&tls.SupportedVersionsExtension{[]uint16{
+						tls.VersionTLS13,
+						tls.VersionTLS12,
+						tls.VersionTLS11,
+						tls.VersionTLS10,
+					}},
+					&tls.SupportedCurvesExtension{[]tls.CurveID{
+						tls.X25519,
+						tls.CurveP256,
+						tls.CurveP384,
+						tls.CurveP521,
+					}},
+					&tls.UtlsPaddingExtension{GetPaddingLen: tls.BoringPaddingStyle},
+				},
+			}
+			tlsConn.ApplyPreset(&clientHelloSpec)
+		} else {
+			tlsConn = tls.UClient(netConn, cfg, tls.HelloChrome_Auto)
+		}
 		netConn = tlsConn
 
 		var err error
